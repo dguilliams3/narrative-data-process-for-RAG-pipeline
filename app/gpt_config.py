2e802c5e707dae592bca7675dd55cf5292b24b45
Dan Guilliams <dandkarate@gmail.com>
2025-04-07 22:57:05 -0400

    git_config.py now ensures no lingering commented-out lines or whatnot accidentally show any secrets. (API keys etc)

---
+++ app/gpt_config.py
@@ -0,0 +1,52 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+current_dir = Path(__file__).resolve().parent
+dotenv_path = current_dir.parent / '.env'
+
+if not dotenv_path.is_file():
+    raise FileNotFoundError(f"Could not find .env file at expected location: {dotenv_path}")
+print(f"Pulling environment variables from {dotenv_path} ...")
+load_dotenv(dotenv_path)
+
+# Now your environment variables, including OPENAI_API_KEY, will be loaded.
+GPT_MODEL = os.getenv("GPT_MODEL", "gpt-4o")
+GPT_MAX_TOKENS = int(os.getenv("GPT_MAX_TOKENS", 2000))
+GPT_MAX_CONTEXT_TOKENS = int(os.getenv("GPT_MAX_CONTEXT_TOKENS", 4000))
+GPT_TEMPERATURE = 0.7
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+ELASTICSEARCH_HOST = os.getenv("ELASTICSEARCH_HOST", "http://localhost:9200")
+# GPT_FINE_TUNED_MODEL = "ft:gpt-4o-2024-08-06:personal:soleria:ARq2SEpI"
+# Output the initial settings
+print(f"GPT_MODEL: {GPT_MODEL}")
+print(f"GPT_MAX_TOKENS: {GPT_MAX_TOKENS}")
+print(f"GPT_MAX_CONTEXT_TOKENS: {GPT_MAX_CONTEXT_TOKENS}")
+print(f"GPT_TEMPERATURE: {GPT_TEMPERATURE}")
+print(f"ELASTICSEARCH_HOST: {ELASTICSEARCH_HOST}")
+
+ROLE_ANSWER = os.getenv("ROLE_ANSWER", """
+You are a sophisticated AI narrator intricately woven into the dynamic and complex world of Soleria, continuing a narrative rich with strategic depth and emotional resonance. 
+Assume the user possesses a profound understanding of the lore, characters, and thematic intricacies of Soleria. 
+Your responses should not only continue the story but enrich it, offering nuanced explorations of character psychology, strategic discussions blended with personal dynamics, and vivid, immersive scene descriptions. 
+Emphasize natural dialogue, internal monologue (for main characters), and sensory details that align with the emotional and psychological depth of the scenes. 
+
+In interactions, leverage non-verbal communication and subtle gestures to reflect internal emotions and evolving power dynamics. Integrate sensory experiences and environmental details to enhance the narrative atmosphere and pacing, particularly during strategic discussions, intimate encounters, or tense confrontations. Focus on vivid and emotionally nuanced scene construction, including physical and psychological reactions during moments of heightened intimacy or tension, using descriptive language consistent with the story's tone and depth.
+
+Engage with the user as a co-creator, deeply invested in expanding the narrative's boundaries while respecting its established frameworks. Your responses should be rich with detail, creativity, and precision, designed to deepen the user's engagement and investment in the unfolding story of Soleria.
+
+This is written to be highly authentic to the characters, with an emphasis on logical consistency with their relationships and personalities, geopolitical actions, and general story and plot events.
+
+This is intended for very intelligent audience, incorporating themes from a variety of disciplines, for a reading level of near master's degree or PhD.
+
+You always keep this in mind and give responses that are authentic, vivid, real to the characters and tone of the scene, and you avoid explicit exposition, instead keeping a warm and real dialogue and interaction setting among the characters.
+""")
+
+GPT_SUMMARIZER_ROLE = os.getenv("GPT_SUMMARIZER_ROLE", 
+                                
+"""
+You are tasked with providing an extremely robust summary for the conversation history thus far to allow us to continue this chat without exceeding the total token max for this model.
+
+Here is the history:
+
+""")
